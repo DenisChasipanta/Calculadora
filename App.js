cec18304320ac6bd35542d6bd3e7b1b895198133
DenisChasipanta <DenisChasipanta@users.noreply.github.com>
2023-12-13 19:10:20 -0500

    Calculadora

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonPress = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  const renderButtons = () => {
+    const buttons = [
+      '7', '8', '9', '/',
+      '4', '5', '6', '*',
+      '1', '2', '3', '-',
+      '0', '.', '=', '+',
+      'C',
+    ];
+
+    return buttons.map((button) => (
+      <TouchableOpacity
+        key={button}
+        style={styles.button}
+        onPress={() => handleButtonPress(button)}
+      >
+        <Text style={styles.buttonText}>{button}</Text>
+      </TouchableOpacity>
+    ));
+  };
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titulo}>
+        <Text>CALCULADORA</Text>
+      </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>{result}</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>{input}</Text>
+      </View>
+      <View style={styles.buttonsContainer}>{renderButtons()}</View>
     </View>
   );
 }
@@ -13,8 +61,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  titulo:{
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#0000ff',
   },
-});
+  resultContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  resultText: {
+    fontSize: 32,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  inputText: {
+    fontSize: 24,
+  },
+  buttonsContainer: {
+    flex: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '25%',
+    height: '20%',
+    backgroundColor: '#f1f1f1',
+    borderWidth: 1,
+    borderColor: '#ff5930',
+  },
+  buttonText: {
+    fontSize: 20,
+  },
+});              
